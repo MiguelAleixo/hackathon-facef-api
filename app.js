10211d82dffce744c2521cacc29fc371cea4db57
Miguel Aleixo <miguel@codebit.com.br>
2019-10-26 16:25:22 -0300

    change package

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 3000;
 const exec = require('child_process').exec;
-const myScript = exec(`json-server -p ${port} db.json`);
+const myScript = exec(`json-server -p ${process.env.PORT} db.json`);
 myScript.stdout.on('data', (data)=>{
     console.log(data); 
 });
